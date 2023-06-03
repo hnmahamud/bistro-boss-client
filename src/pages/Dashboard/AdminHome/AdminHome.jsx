9c0defd6d00471af410d75908585e189ac8be855
hnmahamud <hnmahamud@gmail.com>
2023-06-04 01:25:24 +0600

    added dashboard stats

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,7 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const AdminHome = () => {
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+
+  const { data: stats = {} } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure("/admin-stats");
+      return res.data;
+    },
+  });
   return (
-    <div>
-      <h3>Admin home</h3>
+    <div className="w-full flex flex-col justify-center items-center mt-4">
+      <h3 className="text-3xl mb-4">Hi {user.displayName}</h3>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats.revenue}</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Customers</div>
+          <div className="stat-value">{stats.customers}</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Products</div>
+          <div className="stat-value">{stats.products}</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+        </div>
+      </div>
     </div>
   );
 };
