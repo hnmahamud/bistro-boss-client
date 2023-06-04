007a14f1b28f517cf428ca6f2c30f9e9f0970ee5
hnmahamud <hnmahamud@gmail.com>
2023-06-04 06:46:39 +0600

    implement admin dashboard chart

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -2,6 +2,61 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+// For Bar chart
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+} from "recharts";
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+// For pi chart
+import { PieChart, Pie, ResponsiveContainer } from "recharts";
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const AdminHome = () => {
   const { user } = useAuth();
   const [axiosSecure] = useAxiosSecure();
@@ -13,35 +68,100 @@ const AdminHome = () => {
       return res.data;
     },
   });
+
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["chart-data"],
+    queryFn: async () => {
+      const res = await axiosSecure("/order-stats");
+      return res.data;
+    },
+  });
+
   return (
-    <div className="w-full flex flex-col justify-center items-center mt-4">
-      <h3 className="text-3xl mb-4">Hi {user.displayName}</h3>
-      <div className="stats shadow">
-        <div className="stat">
-          <div className="stat-figure text-secondary"></div>
-          <div className="stat-title">Revenue</div>
-          <div className="stat-value">${stats.revenue}</div>
-        </div>
+    <>
+      <div className="w-full flex flex-col justify-center items-center mt-4">
+        <h3 className="text-3xl mb-4">Hi {user.displayName}</h3>
+        <div className="stats shadow">
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Revenue</div>
+            <div className="stat-value">${stats.revenue}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-figure text-secondary"></div>
-          <div className="stat-title">Customers</div>
-          <div className="stat-value">{stats.customers}</div>
-        </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Customers</div>
+            <div className="stat-value">{stats.customers}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-figure text-secondary"></div>
-          <div className="stat-title">Products</div>
-          <div className="stat-value">{stats.products}</div>
-        </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Products</div>
+            <div className="stat-value">{stats.products}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-figure text-secondary"></div>
-          <div className="stat-title">Orders</div>
-          <div className="stat-value">{stats.orders}</div>
+          <div className="stat">
+            <div className="stat-figure text-secondary"></div>
+            <div className="stat-title">Orders</div>
+            <div className="stat-value">{stats.orders}</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="flex">
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="total"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        <div className="w-1/2">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+              <Legend />
+              <Pie
+                data={chartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="count"
+              >
+                {chartData.map((entry, index) => (
+                  <Cell
+                    name={entry.category}
+                    key={`cell-${index}`}
+                    fill={colors[index % colors.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </>
   );
 };
 
